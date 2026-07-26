aa3c673857fdf45339697c286d73da06bc7e773f
sandhiya1107 <sandhiya111107@gmail.com>
2026-07-26 12:31:00 +0530

    Update H.  5(a).String Handling .java

--- H.  5(a).String Handling .java
+++ H.  5(a).String Handling .java
@@ -1,34 +1,25 @@
-import java.util.Scanner;
-public class CountCharacters {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String str = sc.nextLine();
-        int vowels = 0;
-        int consonants = 0;
-        int digits = 0;
-        int specialChars = 0;
-        str = str.toLowerCase();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (Character.isLetter(ch)) {
-                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                    vowels++;
-                } else {
-                    consonants++;
-                }
-            } else if (Character.isDigit(ch)) {
-                digits++;
+class Solution {
+    public boolean isAnagram(String s, String t) {
 
-            } else if (!Character.isWhitespace(ch)) {
-                specialChars++;
-            }
-        }
-        System.out.println("\n----- Result -----");
-        System.out.println("Vowels: " + vowels);
-        System.out.println("Consonants: " + consonants);
-        System.out.println("Digits: " + digits);
-        System.out.println("Special Characters: " + specialChars);
-        sc.close();
-    }
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int num : count) {
+            if (num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
+
+
